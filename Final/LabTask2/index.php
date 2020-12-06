36210669973a932a7a66dd13f5aa01d8563efef8
bulbul-rasel <58388108+bulbul-rasel@users.noreply.github.com>
2020-12-06 11:09:52 +0600

    Add files via upload

---
+++ Final/LabTask2/index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>CSS Responsive</title>
+		<link rel="stylesheet" href="views/style.css">
+	</head>
+	<body>
+		<header id="top" class="grid">
+			<div class="headBg"></div>
+			<div class="headtext">
+			<h1>Welcome to Acme Web Solutions</h1>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci eum error earum soluta voluptatum nisi laboriosam eos saepe asperiores dolorum.
+			</p>
+			</div>
+		</header>
+		<br>
+		<section id="sec-a" class="grid">
+			<div>
+				<h2 class="review">Web & Application Development</h2>
+				<div>
+					<p class="review">
+						"Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe sint eligendi possimus? Unde officiis magnam laborum ipsa distinctio odio, vero dolores dicta aliquam aperiam repellendus. Perferendis officiis deserunt velit voluptas nobis sequi animi totam, accusantium, ex eius quia, natus quo?"
+					</p>
+				</div>
+			</div>
+		</section>
+		
+		<br>
+		<section id="sec-c" class="grid">
+			<ul>
+				<li>
+					<div class="card">
+						<img src="views/bulbul1.jpg" alt="Web Development">
+						<div class="card-content">
+							<h3 class="card-title">Web Development</h3>
+							<div>
+
+					            <p class="review">
+						"Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe sint eligendi possimus? Unde officiis magnam laborum ipsa distinctio odio, vero dolores dicta aliquam aperiam repellendus. Perferendis officiis deserunt velit voluptas nobis sequi animi totam, accusantium, ex eius quia, natus quo?"
+					            </p>
+				            </div>
+							
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="card">
+						<img src="views/bulbul2.jpg" alt="Mobile Applications">
+						<h3 class="card-title">Mobile Applications</h3>
+						<div>
+
+					        <p class="ourreview">
+						"Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe sint eligendi possimus? Unde officiis magnam laborum ipsa distinctio odio, vero dolores dicta aliquam aperiam repellendus. Perferendis officiis deserunt velit voluptas nobis sequi animi totam, accusantium, ex eius quia, natus quo?"
+					        </p>
+				        </div>
+						
+					</div>
+					
+				</li>
+				<li>
+					<div class="card">
+						<img src="views/bulbul.jpg" alt="Tech Marketing">
+						<h3 class="card-title">Tech Marketing</h3>
+						<div>
+
+					        <p class="ourreview">
+						"Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe sint eligendi possimus? Unde officiis magnam laborum ipsa distinctio odio, vero dolores dicta aliquam aperiam repellendus. Perferendis officiis deserunt velit voluptas nobis sequi animi totam, accusantium, ex eius quia, natus quo?"
+					        </p>
+				        </div>
+						
+					</div>
+					
+				</li>
+			</ul>
+			
+		</section>
+		<br>
+		<section id="sec-d" class="grid">
+			<h2 class="about">We handle all of your digital needs</h2>
+				<div>
+					<p class="about">
+						Along with his streetwise snail crew and new human friends, Turbo, an underdog snail, takes on the biggest challenge ever to make their dreams come true.
+					</p>
+		</section>
+		<br>
+		<section id="sec-e" class="grid2">
+			<div class="box">
+			<h2 class="contact">Contact Us</h2>
+			<p>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, suscipit. Rerum ducimus a quod, ut et voluptas obcaecati unde fuga.
+				<a href="contact@acmewebsolutions.test"></a>
+			</p>
+			</div>
+			<div class="box">
+			<h2 class="about">About Our Company</h2>
+			<p>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio earum porro deserunt, deleniti, quae facere repudiandae, officiis est exercitationem nobis iusto doloremque! Soluta excepturi in aut suscipit amet temporibus quo?
+			</p>
+			</div>
+		</section>
+		<br>
+		<br>
+		<div>
+			<center>
+				Acme Web Solutions
+			</center>
+		</div>
+	</body>
+</html>
